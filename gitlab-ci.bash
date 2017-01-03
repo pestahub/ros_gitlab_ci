a733bffe3a1398c7384803811889d5342bd4bcfd
Victor Lamoine <victor.lamoine@gmail.com>
2017-01-03 11:09:32 +0100

    Split scripts

--- gitlab-ci.bash
+++ gitlab-ci.bash
@@ -1,101 +1,28 @@
 #!/bin/bash
 
-# Determine ROS_DISTRO
-#---------------------
-ROS_DISTRO=$(ls /opt/ros/)
-
-if [ -z "$ROS_DISTRO" ]; then
-  echo "No ROS distribution was found in /opt/ros/. Aborting!"
-  exit -1
-fi
-
-# Source ROS
-#-----------
-source /opt/ros/$ROS_DISTRO/setup.bash >/dev/null
-
-# Install catkin tools # https://catkin-tools.readthedocs.io/en/latest/installing.html
-#---------------------
-apt-get update >/dev/null
-apt-get install -qq wget >/dev/null
-sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list' >/dev/null
-wget http://packages.ros.org/ros.key -O - | apt-key add - >/dev/null
-apt-get update >/dev/null
-apt-get install -qq python-catkin-tools xterm >/dev/null
-export TERM=xterm # Makes catkin build output less ugly
-
-# Install ROS packages required by the user
-#------------------------------------------
-# Split packages into package list
-IFS=' ' read -ra PACKAGES <<< "$ROS_PACKAGES_TO_INSTALL"
-# Clear packages list
-ROS_PACKAGES_TO_INSTALL=""
-
-# Append "ros-kinetic-" (eg for Kinetic) before the package name
-# and append in the packages list
-for package in "${PACKAGES[@]}"; do
-    ROS_PACKAGES_TO_INSTALL="${ROS_PACKAGES_TO_INSTALL} ros-$ROS_DISTRO-$package"
-done
-
-# Install the packages
-apt-get install -qq $ROS_PACKAGES_TO_INSTALL >/dev/null
-
-# Enable global C++11 if required by the user
-#--------------------------------------------
-if [ ! -z ${GLOBAL_C11+x} ]; then
-  echo "Enabling C++11 globally"
-  export CXXFLAGS="$CXXFLAGS -std=c++11"
-fi
-
-# Display system information
-#---------------------------
-echo "##############################################"
-uname -a
-lsb_release -a
-gcc --version
-echo "CXXFLAGS = $CXXFLAGS"
-cmake --version
-echo "##############################################"
-
-# Prepare build
-#--------------
-# https://docs.gitlab.com/ce/ci/variables/README.html#predefined-variables-environment-variables
-
-# Does this project have a wstool install file?
-rosinstall_file=$(find $CI_PROJECT_DIR -maxdepth 2 -type f -name "*.rosinstall")
-
-if [ -z "$rosinstall_file" ]; then
-  cd $CI_PROJECT_DIR/..
-  mkdir -p src
-  # Copy current directory into a src directory
-  # Don't move the original clone or GitLab CI fails!
-  cp -r $CI_PROJECT_DIR src/
-  mv src $CI_PROJECT_DIR
-  cd $CI_PROJECT_DIR
-else
-  echo "Using wstool file $rosinstall_file"
-  # Install wstool
-  apt-get install -qq python-wstool >/dev/null
-  # Create workspace
-  wstool init src $rosinstall_file
-  wstool update -t src
-fi
+# https://docs.gitlab.com/ce/ci/variables/README.html
 
 # If self testing
 #----------------
-# This happens only if ros_gitlab_ci is testing himself, not in user repositories!
-
-if [ ${CI_PROJECT_URL} == "https://gitlab.com/VictorLamoine/ros_gitlab_ci" ]; then
+if [ ${CI_PROJECT_URL} == "https://gitlab.com/VictorLamoine/$CI_PROJECT_NAME" ]; then
   echo "##############################################"
-  echo "Self testing!"
+  # Go into the sub ROS GitLab CI repository
+  cd $CI_PROJECT_DIR/$CI_PROJECT_NAME
+
   # Switch to the branch we want to test
-  git checkout ${CI_BUILD_REF_NAME}
+  git checkout ${CI_BUILD_REF_NAME} &>/dev/null
+  echo "Self testing, ${CI_PROJECT_URL}"
   echo $'Current branch is:\n'"$(git branch)"
 
-  cd $CI_PROJECT_DIR/src
   # We create a package beginner_tutorials so that the catkin workspace is not empty
+  mkdir $CI_PROJECT_DIR/src
+  cd $CI_PROJECT_DIR/src
   catkin_create_pkg beginner_tutorials std_msgs rospy roscpp
   cd $CI_PROJECT_DIR
-else
-    echo "Not self testing"
+  echo "##############################################"
 fi
+
+# Source the gitlab-ros script from the sub GitLab repository
+# This repository has the right branch
+source $CI_PROJECT_NAME/gitlab-ros.bash
 
---
+++ gitlab-ros.bash
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# Determine ROS_DISTRO
+#---------------------
+ROS_DISTRO=$(ls /opt/ros/)
+
+if [ -z "$ROS_DISTRO" ]; then
+  echo "No ROS distribution was found in /opt/ros/. Aborting!"
+  exit -1
+fi
+
+# Source ROS
+#-----------
+source /opt/ros/$ROS_DISTRO/setup.bash >/dev/null
+
+# Install catkin tools # https://catkin-tools.readthedocs.io/en/latest/installing.html
+#---------------------
+apt-get update >/dev/null
+apt-get install -qq wget >/dev/null
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list' >/dev/null
+wget http://packages.ros.org/ros.key -O - | apt-key add - >/dev/null
+apt-get update >/dev/null
+apt-get install -qq python-catkin-tools xterm >/dev/null
+export TERM=xterm # Makes catkin build output less ugly
+
+# Install ROS packages required by the user
+#------------------------------------------
+# Split packages into package list
+IFS=' ' read -ra PACKAGES <<< "$ROS_PACKAGES_TO_INSTALL"
+# Clear packages list
+ROS_PACKAGES_TO_INSTALL=""
+
+# Append "ros-kinetic-" (eg for Kinetic) before the package name
+# and append in the packages list
+for package in "${PACKAGES[@]}"; do
+    ROS_PACKAGES_TO_INSTALL="${ROS_PACKAGES_TO_INSTALL} ros-$ROS_DISTRO-$package"
+done
+
+# Install the packages
+apt-get install -qq $ROS_PACKAGES_TO_INSTALL >/dev/null
+
+# Enable global C++11 if required by the user
+#--------------------------------------------
+if [ ! -z ${GLOBAL_C11+x} ]; then
+  echo "Enabling C++11 globally"
+  export CXXFLAGS="$CXXFLAGS -std=c++11"
+fi
+
+# Display system information
+#---------------------------
+echo "##############################################"
+uname -a
+lsb_release -a
+gcc --version
+echo "CXXFLAGS = $CXXFLAGS"
+cmake --version
+echo "##############################################"
+
+# Prepare build
+#--------------
+# https://docs.gitlab.com/ce/ci/variables/README.html#predefined-variables-environment-variables
+
+# Does this project have a wstool install file?
+rosinstall_file=$(find $CI_PROJECT_DIR -maxdepth 2 -type f -name "*.rosinstall")
+
+if [ -z "$rosinstall_file" ]; then
+  cd $CI_PROJECT_DIR/..
+  mkdir -p src
+  # Copy current directory into a src directory
+  # Don't move the original clone or GitLab CI fails!
+  cp -r $CI_PROJECT_DIR src/
+  mv src $CI_PROJECT_DIR
+  cd $CI_PROJECT_DIR
+else
+  echo "Using wstool file $rosinstall_file"
+  # Install wstool
+  apt-get install -qq python-wstool >/dev/null
+  # Create workspace
+  wstool init src $rosinstall_file
+  wstool update -t src
+fi
+
