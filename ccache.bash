70dd7e33d420c16026b74fddd5edc639046aaec8
Victor Lamoine <victor.lamoine@gmail.com>
2019-02-08 10:43:25 +0000

    Fix CI_PROJECT_DIR variable

--- ccache.bash
+++ ccache.bash
@@ -4,7 +4,7 @@ apt-get install -qq ccache
 export PATH="/usr/lib/ccache:$PATH"
 
 # Setup path for ccache
-export CCACHE_DIR=${$CI_PROJECT_DIR}/ccache
+export CCACHE_DIR=${CI_PROJECT_DIR}/ccache
 
 # You need to add this to your GitLab CI config file:
 #cache:
