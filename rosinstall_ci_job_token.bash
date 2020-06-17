e0eae5a57108ece5fa86da3956b642f83e16d682
Vincent sagri <vincent.rousseau@sabi-agri.com>
2020-06-17 11:22:22 +0000

    Multi rosinstall

--- rosinstall_ci_job_token.bash
+++ rosinstall_ci_job_token.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt install -qq -y gettext >/dev/null
-sed -i 's/gitlab\.com/gitlab-ci-token:\$\{CI_JOB_TOKEN\}\@gitlab\.com/g' $1
+sed -i 's/https:\/\/gitlab\./https:\/\/gitlab-ci-token:\$\{CI_JOB_TOKEN\}\@gitlab\./g' $1
 # Replace CI_JOB_TOKEN by its content
 envsubst < $1 > tmp.rosinstall
 rm $1
